d4ee98c025e702f1224ab73becb9c399ade70fde
miiitomi <ytommyrad@gmail.com>
2021-11-03 19:35:17 +0900

    modify 4_9

--- chapter4/4_9.cpp
+++ chapter4/4_9.cpp
@@ -3,38 +3,56 @@
 #include <cmath>
 #include <random>
 #include <chrono>
-#include <iomanip>
 using namespace std;
 
-int main() {
-    int N = pow(10, 7);
-    vector<int> int_devidend(N), int_devisor(N);
-    vector<float> float_devidend(N), float_devisor(N);
 
-    mt19937 mt(0);
-    uniform_int_distribution<int> uid(1, 100000000);
-    for (int i = 0; i < N; i++) {
-        int_devidend[i] = uid(mt);
-        int_devisor[i] = uid(mt);
-        float_devidend[i] = (float)int_devidend[i];
-        float_devisor[i] = (float)float_devisor[i];
-    }
-
+template<typename T> void iterative_devide(vector<T> &devidend, vector<T> &devisor) {
+    int N = (int)devidend.size();
     chrono::system_clock::time_point start, end;
 
     start = chrono::system_clock::now();
     for (int i = 0; i < N; i++) {
-        int_devidend[i] / int_devisor[i];
+       devidend[i] / devisor[i];
     }
     end = chrono::system_clock::now();
-    double elapsed = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-    cout << "int  : " << elapsed << " [ms]" << endl;
+    int elapsed = chrono::duration_cast<chrono::milliseconds>(end-start).count();
+    cout  << elapsed << " [ms]" << endl;
+}
 
-    start = chrono::system_clock::now();
+
+int main() {
+    int N = pow(10, 7);
+
+    vector<int> int_devidend(N), int_devisor(N);  // 32-bit integer
+    vector<long long> ll_devidend(N), ll_devisor(N);  // 64-bit integer
+    vector<float> float_devidend(N), float_devisor(N);  // 32-bit floating point number
+    vector<double> double_devidend(N), double_devisor(N);  // 64-bit floating point number
+
+    // generate devidend and devisor randomly
+    mt19937 mt(0);
+    uniform_int_distribution<int> uid(1, (int)pow(10, 8));
     for (int i = 0; i < N; i++) {
-        float_devidend[i] / float_devisor[i];
+        int a = uid(mt);
+        int b = uid(mt);
+        int_devidend[i] = (int)a;
+        int_devisor[i] = (int)b;
+        ll_devidend[i] = (long long)a;
+        ll_devisor[i] = (long long)b;
+        float_devidend[i] = (float)a;
+        float_devisor[i] = (float)b;
+        double_devidend[i] = (double)a;
+        double_devisor[i] = (double)b;
     }
-    end = chrono::system_clock::now();
-    elapsed = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-    cout << "float: " << elapsed << " [ms]" << endl;
+
+    cout << "int   : ";
+    iterative_devide(int_devidend, int_devisor);
+
+    cout << "ll    : ";
+    iterative_devide(ll_devidend, ll_devisor);
+
+    cout << "float : ";
+    iterative_devide(float_devidend, float_devisor);
+
+    cout << "double: ";
+    iterative_devide(double_devidend, double_devisor);
 }
